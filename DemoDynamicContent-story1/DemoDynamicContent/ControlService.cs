c26cd79ebd44767af8af6fb1a9cc0172884e2bd0
Ev <ev.uklad@procoders.com.au>
2020-10-08 14:51:51 +1100

    added story 1

---
+++ DemoDynamicContent-story1/DemoDynamicContent/ControlService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoDynamicContent
+{
+    public class ControlService
+    {
+        public List<ControlDetails> GetControls()
+        {
+            var result = new List<ControlDetails>();
+            result.Add(new ControlDetails { Type = "TextEdit", Label = "First Name", IsRequired = true });
+            result.Add(new ControlDetails { Type = "TextEdit", Label = "Last Name", IsRequired = true });
+            result.Add(new ControlDetails { Type = "DateEdit", Label = "Birth Date", IsRequired = false });
+            return result;
+        }
+    }
+}
